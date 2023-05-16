b2388601dbd3c4a9d314f82bff6a1164dfbf41b2
Daniela Moreno <dani9292@hotmail.com>
2023-05-16 11:16:38 -0500

    Add btn

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import '../styles/missions.css';
 
 const Missions = () => (
-  <div>this returns Missions</div>
+  <Container className="custom-container">
+    <Row>
+      <Col xs={2} className="custom-col">
+        hello
+      </Col>
+      <Col className="custom-col">
+        hello
+      </Col>
+    </Row>
+    <Row>
+      <Col  xs={2}>
+      <Button  variant="outline-primary" size="xs" >Make your reservation</Button>
+      </Col>
+     
+    </Row>
+  </Container>
 );
 
 export default Missions;
