059cfb409ee79609d5c5329d6b9b1c635e25e997
Juan Sanchez <juansan1993@hotmail.com>
2023-05-16 15:42:31 -0500

    Add data from the api for rockets

--- src/components/Rockets.jsx
+++ src/components/Rockets.jsx
@@ -1,5 +1,28 @@
-import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { getRockets } from '../redux/rocketsSlice';
 
-const Rockets = () => <div>this returns Rockets</div>;
+function Rockets() {
+  const {
+    name,
+    id,
+    type,
+    flickrImages,
+  } = useSelector((state) => state.rockets);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getRockets());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <h1>Rockets</h1>
+      <h3>{name}</h3>
+      <h2>{id}</h2>
+      <h2>{type}</h2>
+      <img src={flickrImages} alt="test" />
+    </div>
+  );
+}
 
 export default Rockets;
